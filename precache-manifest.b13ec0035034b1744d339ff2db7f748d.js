4117d519bc020ac9b9013a3386ecb80ee5dd0724
Nguyen Phat <phatnt.uit@gmail.com>
2019-09-27 00:37:58 +0700

    First deploy with gh-pages

---
+++ precache-manifest.b13ec0035034b1744d339ff2db7f748d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2bcc4933daecba293feeef83449dd9c",
+    "url": "/hello-react-tsx/index.html"
+  },
+  {
+    "revision": "089fb368e79838b9035a",
+    "url": "/hello-react-tsx/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "bd5231468496a04d9dad",
+    "url": "/hello-react-tsx/static/js/2.600a7e5b.chunk.js"
+  },
+  {
+    "revision": "089fb368e79838b9035a",
+    "url": "/hello-react-tsx/static/js/main.3da8a33a.chunk.js"
+  },
+  {
+    "revision": "14467fb15ba678b52e17",
+    "url": "/hello-react-tsx/static/js/runtime-main.48598004.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/hello-react-tsx/static/media/logo.25bf045c.svg"
+  }
+]);
